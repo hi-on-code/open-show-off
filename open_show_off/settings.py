06cca3bc82864cdd9754e31045470cad395a2c00
ssurinen <sampath.surineni@oracle.com>
2018-10-31 08:39:25 +0530

    added static root to test

--- open_show_off/settings.py
+++ open_show_off/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
