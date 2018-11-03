8419cef1ec0f3c031dffc2fdf3891d9a05ae0580
ssurinen <sampath.surineni@oracle.com>
2018-11-03 09:18:56 +0530

    deleted static file dir

--- open_show_off/settings.py
+++ open_show_off/settings.py
@@ -127,18 +127,18 @@ WSGI_APPLICATION = 'open_show_off.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
