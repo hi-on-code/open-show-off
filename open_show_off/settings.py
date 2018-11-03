1b6d464a349ac214e8d4166e29287e1ae07d482e
ssurinen <sampath.surineni@oracle.com>
2018-11-03 09:31:55 +0530

    added missing index.html

--- open_show_off/settings.py
+++ open_show_off/settings.py
@@ -127,18 +127,21 @@ WSGI_APPLICATION = 'open_show_off.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+ENVIRON = config('ENVIRON')
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+if ENVIRON == 'PROD':
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
+    }
+elif ENVIRON == 'DEV':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
